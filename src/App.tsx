fa42fad3105d74af464387c654d7a36a36bdf877
MILTON LUIZ SINHORINI JUNIOR <milton.junior@alunos.sc.senac.br>
2025-08-04 20:44:38 -0300

    um texto legal

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h2>My first page in GitHub pages - Milton Junior</h2>
+      <h3>Um texto Legal</h3>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
